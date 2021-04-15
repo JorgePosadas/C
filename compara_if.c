2449d1a63eb69df2641caf90d57b000dc5d00ecd
JorgePosadas <jposadasp@gmail.com>
2021-04-15 14:13:26 -0500

    Create compara_if.c

---
+++ compara_if.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int x=0;
+int y=0;
+
+int main(void){
+printf("Introduce un número: ");
+scanf("%d", &x);
+printf("Introduce un número: ");
+scanf("%d", &y);
+
+if(x==y){
+    printf("\nEsto es el primer IF\n", x, y);
+    printf("%d es igual que %d", x, y);
+}
+else{
+if(x>y){
+    printf("\nEsto es el segundo IF\n", x, y);
+    printf("%d es mayor que %d", x, y);
+}
+else{
+        printf("\nEsto es el tercer IF\n", x, y);
+    printf("%d es mayor que %d", y, x);
+    }
+}
+}
+printf("\nFin del programa\n");
+return 0;
