98ffbd3b62a29908cc9cd2781125724ce2202199
JorgePosadas <jposadasp@gmail.com>
2021-04-22 00:23:04 -0500

    Update compara_if.c

--- compara_if.c
+++ compara_if.c
@@ -4,25 +4,27 @@ int x=0;
 int y=0;
 
 int main(void){
+
 printf("Introduce un número: ");
-scanf("%d", &x);
+scanf("%d",&x);
 printf("Introduce un número: ");
-scanf("%d", &y);
+scanf("%d",&y);
 
 if(x==y){
-    printf("\nEsto es el primer IF\n", x, y);
+    printf("\nEsto es el primer IF\n");
     printf("%d es igual que %d", x, y);
 }
 else{
 if(x>y){
-    printf("\nEsto es el segundo IF\n", x, y);
+    printf("\nEsto es el segundo IF\n");
     printf("%d es mayor que %d", x, y);
 }
 else{
-        printf("\nEsto es el tercer IF\n", x, y);
+    printf("\nEsto es el tercer IF\n");
     printf("%d es mayor que %d", y, x);
     }
 }
-}
 printf("\nFin del programa\n");
 return 0;
+}
+
