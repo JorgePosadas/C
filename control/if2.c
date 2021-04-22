3a9c583c3bf74e5abf9e1bbc0c77efd1146c6283
JorgePosadas <jposadasp@gmail.com>
2021-04-22 00:22:53 -0500

    Update if2.c

--- control/if2.c
+++ control/if2.c
@@ -2,24 +2,28 @@
 int main()
 {
 	int edad;
-	printf("Introduce tu edad: ");
+	printf("Introduce tu edad: [0-150]");
 	scanf("%d",&edad);
 
 	if (edad >17 && edad <35)
 	{
 		 printf("Tienes %d anios, eres mayor de edad",edad);
 	}
-	else if (edad < 17)
+	else if (edad >= 0 && edad <= 17)
 	{
 		 printf("Tienes %d anios, eres menor de edad",edad);
 	}
-	else if (edad >= 35)
+	else if (edad >= 35 && edad <=150)
 	{
-		 printf("Tienes %d anios, eres chavo-ruco",edad);
+		 printf("Tienes %d anios, eres adulto",edad);
 	}
 	else
 	{
-		 printf("Tienes %d anios, ",edad);
+		 printf("Tienes %d anios, eres adulto-mayor",edad);
+	}
+}
+    else{
+		
 	}
   return 0;
 } 	
