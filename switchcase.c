d28ae5fd58fd09d5854656324f1b73f37b4d5757
JorgePosadas <jposadasp@gmail.com>
2021-04-29 15:39:30 -0500

    Update switchcase.c

--- switchcase.c
+++ switchcase.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
-
-/* 
-
-1.- Crear repositorio
-2.- Crear programa
-
-   2.1.- Divisa a convertir
-   2.2.- Cantidad de pesos mexicanos a convertir
-   2.3.- Imprimir la conversión de divisas
-3. Subir a repositorio
-4. Compartir enlace
-*/
 
 int main(){
 
@@ -21,54 +10,75 @@ float b = 0;
 int operacion, opcion;
 float resultadoSuma;
 
+
 printf("----------------------------------------");
-printf("\n¿Que deseas hacer?\n");
-printf("1.- Realizar operaciones \n2.- Salir\n");
+printf("\n�Que deseas hacer?\n");
+printf(" 1.- Realizar operaciones \n 2.- Salir\n");
 scanf("%d", &opcion);
 
-if (operacion != 1){
-    printf("Salir\n");
+
+if (opcion != 1){
+    printf("\nPrograma terminado\n");
+	printf("----------------------------------------");
     exit(0);
 }else{
+printf("----------------------------------------");
+printf("\nQue operacion deseas?:\n");
+printf(" 1.- Suma \n 2.- Resta \n 3.- Multiplicacion \n 4.- Division \n 5.- Raiz Cuadrada \n 6.- Potencia\n 7.- Salir\n");
+scanf("%d", &operacion);
+}
+if(operacion ==7){
+printf("\nPrograma terminado\n");
+printf("----------------------------------------");
+exit(0);
+}
+if(operacion ==5){
+printf("----------------------------------------");
+printf("\nDame el numero: ");
+scanf("%f", &a);
+}
+if(operacion !=5){
+
 
 //Ingresar los datos (a,b)
-printf("Introduce el primer numero:   ");
+printf("----------------------------------------");
+printf("\nIntroduce el primer numero:  ");
 scanf("%f", &a);
-printf("Introduce el segundo numero:   ");
+printf("\nIntroduce el segundo numero:  ");
 scanf("%f", &b);
+}
 
-//Elegir operación (+,-,*,/)
-printf("Que operacion deseas?:   ");
-printf("1.- Suma \n2.- Resta \n3.- Multiplicacion \n4.- Division \n5.- Salir\n");
-scanf("%f", &operacion);
 
 //Imprimir el resultado
 switch ( operacion ){
     case 1:
-    resultadoSuma = a + b;
-    printf("El resultado de %0.2f + %0.2f = %f\n", a, b, resultadoSuma);
+    printf("\nEl resultado de %0.2f + %0.2f = %f\n", a, b, resultadoSuma);
         break;
     case 2:
-    printf("El resultado de %0.2f - %0.2f = %0.2f\n", a, b, a - b);
+    printf("\nEl resultado de %0.2f - %0.2f = %0.2f\n", a, b, a - b);
         break;
     case 3:
-    printf("El resultado de %0.2f * %0.2f = %0.2f\n", a, b, a * b);
+    printf("\nEl resultado de %0.2f * %0.2f = %0.2f\n", a, b, a * b);
         break;
     case 4:
     if( b == 0){
-    printf("\tLa division entre %0.2f no está definida\n", b);
+    printf("\nLa division entre %0.2f no esta definida\n", b);
     }else{
-    printf("El resultado de %0.2f / %0.2f = %0.2f\n", a, b, a / b);
+    printf("\nEl resultado de %0.2f / %0.2f = %0.2f\n", a, b, a / b);
     }
     break;
     case 5:
-    printf("Salir\n");
-    exit(0);
-    default:
-    printf("Elige una opcion valida\n");
-}
-printf("Printf fuera del switch");
-}
-printf("Printf fuera del else");
-return 0;
+    if(a < 0){
+    printf("\nLa raiz cuadrada de un numero negativo no esta definida\n");
+    }else{
+    printf("\nLa raiz cuadrada de %.2f es %.2f", a, sqrt(a));
+	}
+	break;
+    case 6:
+    printf("\nEl resultado de %.2f elevado a la %.2f es %.2f\n", a, b, pow(a, b));
+        break;
+	case 7:
+	exit(0);
+    default: printf("Elige una opcion valida\n");
+}while(operacion != 7);
 }
